66e0e5558de0b28369127b1eb82d84b3cfc21151
evan <evan@debian.myguest.virtualbox.org>
2026-01-13 16:50:46 -0600

    another project idea!

---
+++ projects1/stackSmashingExample.asm
@@ -0,0 +1,13 @@
+;I learned about stack smashing via the endbr64 intruction. Stack smashing is
+;a hacking method- quite historical by now- that involves executing outside 
+;code by using tools to put it those instructions into the stack. 
+;
+;later, after security implementations, return oriented programming and jump
+;oriented programming took use of the instructions ALREADY IN THE PROGRAM in
+;order to execute malicious code. It is incredibly facinating stuff, and I will
+;here- make an example of stack smashing via a program and some outside tools.
+;
+;for now, more research is needed.
+;
+;author Evan Delabarre
+;1/13/2026
